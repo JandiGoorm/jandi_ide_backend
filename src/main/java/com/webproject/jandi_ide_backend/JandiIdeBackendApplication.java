9021ed73bcf23aa80b193df84a1fea9c9a25da4d
kyj0503 <heroria0503@gmail.com>
2025-04-04 14:45:14 +0900

    최초 커밋

---
+++ src/main/java/com/webproject/jandi_ide_backend/JandiIdeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webproject.jandi_ide_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JandiIdeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JandiIdeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webproject/jandi_ide_backend/JandiIdeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webproject.jandi_ide_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JandiIdeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
