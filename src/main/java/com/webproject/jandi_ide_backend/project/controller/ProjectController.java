3cb9cb6c513d14867a219672fe0dd3c85eb103be
Yoonhwi <ush0105@naver.com>
2025-04-16 20:57:03 +0900

    FEAT: 프로젝트 tree 및 blob조회 추가

--- src/main/java/com/webproject/jandi_ide_backend/project/controller/ProjectController.java
+++ src/main/java/com/webproject/jandi_ide_backend/project/controller/ProjectController.java
@@ -2,14 +2,16 @@ package com.webproject.jandi_ide_backend.project.controller;
 
 import com.webproject.jandi_ide_backend.global.error.CustomErrorCodes;
 import com.webproject.jandi_ide_backend.global.error.CustomException;
+import com.webproject.jandi_ide_backend.project.dto.BlobResponseDTO;
 import com.webproject.jandi_ide_backend.project.dto.ProjectCreateRequestDTO;
 import com.webproject.jandi_ide_backend.project.dto.ProjectResponseDTO;
 import com.webproject.jandi_ide_backend.project.dto.ProjectUpdateRequestDTO;
+import com.webproject.jandi_ide_backend.project.entity.Project;
+import com.webproject.jandi_ide_backend.project.repository.ProjectRepository;
 import com.webproject.jandi_ide_backend.project.service.ProjectService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -19,6 +21,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+
 /**
  * 대표 프로젝트 관련 API를 처리하는 컨트롤러.
  */
@@ -27,16 +31,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/projects")
 public class ProjectController {
     private final ProjectService projectService;
+    private final ProjectRepository projectRepository;
 
-    public ProjectController(ProjectService projectService){
+    public ProjectController(ProjectService projectService, ProjectRepository projectRepository) {
         this.projectService = projectService;
+        this.projectRepository = projectRepository;
     }
 
     @PostMapping
     @Operation(summary = "대표 프로젝트 추가", description = "자신의 대표 프로젝트를 추가합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "추가 성공",
-                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = ProjectResponseDTO.class)))
+                    content = @Content(schema = @Schema(implementation = ProjectResponseDTO.class))
             ),
     })
     public ResponseEntity<ProjectResponseDTO> postProject(
@@ -56,6 +62,34 @@ public class ProjectController {
 
         String accessToken = token.replace("Bearer ", "").trim();
         ProjectResponseDTO projectResponseDTO = projectService.postProject(accessToken, requestDTO);
+        return ResponseEntity.ok(projectResponseDTO);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "특정 프로젝트 조회", description = "특정 프로젝트를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공",
+                    content = @Content(schema = @Schema(implementation = Object.class))
+            ),
+    })
+    public ResponseEntity<?> getProject(
+            @Parameter(
+                    name = "Authorization",
+                    description = "액세스 토큰을 입력해주세요",
+                    required = true,
+                    example = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+            )
+            @RequestHeader("Authorization") String token,
+            @PathVariable int id
+    ){
+        if (token == null || token.isBlank() || !token.startsWith("Bearer ")) {
+            throw new CustomException(CustomErrorCodes.INVALID_JWT_TOKEN);
+        }
+
+        String accessToken = token.replace("Bearer ", "").trim();
+        Project project = projectRepository.findById(id)
+                .orElseThrow(() -> new CustomException(CustomErrorCodes.PROJECT_NOT_FOUND));
+        Object projectResponseDTO = projectService.getProject(accessToken,project.getOwner().getGithubUsername(),project.getGithubName());
         return ResponseEntity.ok(projectResponseDTO);
     }
 
@@ -116,5 +150,35 @@ public class ProjectController {
         projectService.deleteProject(accessToken, id);
 
         return ResponseEntity.ok("프로젝트 삭제 성공");
+    }
+
+    @GetMapping("/{id}/blob")
+    @Operation(summary = "특정 프로젝트의 blob 조회", description = "특정 프로젝트의 blob을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공",
+                    content = @Content(schema = @Schema(implementation = BlobResponseDTO.class))
+            ),
+    })
+    public ResponseEntity<BlobResponseDTO> getProjectBlob(
+            @Parameter(
+                    name = "Authorization",
+                    description = "액세스 토큰을 입력해주세요",
+                    required = true,
+                    example = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+            )
+            @RequestHeader("Authorization") String token,
+            @PathVariable int id,
+            @RequestParam(value = "sha", required = true) String sha
+    ){
+        if (token == null || token.isBlank() || !token.startsWith("Bearer ")) {
+            throw new CustomException(CustomErrorCodes.INVALID_JWT_TOKEN);
+        }
+
+        String accessToken = token.replace("Bearer ", "").trim();
+        projectRepository.findById(id)
+                .orElseThrow(() -> new CustomException(CustomErrorCodes.PROJECT_NOT_FOUND));
+
+        BlobResponseDTO projectResponseDTO = projectService.getProjectBlob(accessToken,id, sha);
+        return ResponseEntity.ok(projectResponseDTO);
     }
 }
--- src/main/java/com/webproject/jandi_ide_backend/project/service/ProjectService.java
+++ src/main/java/com/webproject/jandi_ide_backend/project/service/ProjectService.java
@@ -2,6 +2,7 @@ package com.webproject.jandi_ide_backend.project.service;
 
 import com.webproject.jandi_ide_backend.global.error.CustomErrorCodes;
 import com.webproject.jandi_ide_backend.global.error.CustomException;
+import com.webproject.jandi_ide_backend.project.dto.BlobResponseDTO;
 import com.webproject.jandi_ide_backend.project.dto.ProjectCreateRequestDTO;
 import com.webproject.jandi_ide_backend.project.dto.ProjectResponseDTO;
 import com.webproject.jandi_ide_backend.project.dto.ProjectUpdateRequestDTO;
@@ -12,7 +13,14 @@ import com.webproject.jandi_ide_backend.security.TokenInfo;
 import com.webproject.jandi_ide_backend.user.entity.User;
 import com.webproject.jandi_ide_backend.user.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -29,7 +37,44 @@ public class ProjectService {
     }
 
     /**
-     * 대표 프로젝트 추가
+     * 특정 유저의 대표 프로젝트 조회
+     * @param accessToken: 사용자 액세스 토큰
+     * @param id: 사용자 ID
+     * @return List<ProjectResponseDTO>
+     */
+    public List<ProjectResponseDTO> getProjects(String accessToken , Integer id) {
+        // 1. 토큰 검증
+        jwtTokenProvider.decodeToken(accessToken);
+
+        // 2. 사용자 정보 조회
+        User user = userRepository.findById(id).orElseThrow(() -> new CustomException(CustomErrorCodes.USER_NOT_FOUND));
+
+        // 3. 프로젝트 정보 조회
+        List<Project> projects = projectRepository.findByOwner_Id(user.getId());
+
+        // 4. 응답 DTO 반환
+        return projects.stream().map(this::convertToDTO).toList();
+    }
+
+    /**
+     * 특정 유저의 레포지토리 트리 조회
+     * @param accessToken: 사용자 액세스 토큰
+     * @param githubUsername: 깃허브 사용자 이름
+     * @param githubReponame: 깃허브 레포지토리 이름
+     * @return 레포지토리 트리 정보
+     */
+    public Object getProject(String accessToken, String githubUsername, String githubReponame) {
+        TokenInfo tokenInfo = jwtTokenProvider.decodeToken(accessToken);
+        String githubToken = tokenInfo.getGithubToken();
+
+        String defaultBranch = getDefaultBranch(githubToken, githubUsername, githubReponame);
+        log.info("Default branch: {}", defaultBranch);
+
+        return getRepoTrees(defaultBranch,githubUsername,githubReponame,githubToken);
+    }
+
+    /**
+     * 내 대표 프로젝트 추가
      * @param accessToken: 사용자 액세스 토큰
 //     * @param id: 사용자 ID
      * @param requestDTO: 프로젝트 요청 DTO
@@ -63,7 +108,7 @@ public class ProjectService {
     }
 
     /**
-     * 대표 프로젝트 수정
+     * 내 대표 프로젝트 수정
      * @param accessToken: 사용자 액세스 토큰
      * @param id: 프로젝트 ID
      * @param requestDTO: 프로젝트 요청 DTO
@@ -98,6 +143,11 @@ public class ProjectService {
         return convertToDTO(project);
     }
 
+    /**
+     * 내 대표 프로젝트 삭제
+     * @param accessToken: 사용자 액세스 토큰
+     * @param id: 프로젝트 ID
+     */
     public void deleteProject(String accessToken,Integer id){
         // 1. 토큰 검증
         TokenInfo tokenInfo = jwtTokenProvider.decodeToken(accessToken);
@@ -120,6 +170,100 @@ public class ProjectService {
         }
     }
 
+    public BlobResponseDTO getProjectBlob(String accessToken,Integer id, String sha){
+        TokenInfo tokenInfo = jwtTokenProvider.decodeToken(accessToken);
+        String githubId = tokenInfo.getGithubId();
+
+        User user = userRepository.findByGithubId(githubId).orElseThrow(() -> new CustomException(CustomErrorCodes.USER_NOT_FOUND));
+        Project project = projectRepository.findById(id).orElseThrow(() -> new CustomException(CustomErrorCodes.PROJECT_NOT_FOUND));
+
+        // 1. 레포지토리의 default branch 조회
+        String githubUsername = user.getGithubUsername();
+        String githubReponame = project.getGithubName();
+        String githubToken = tokenInfo.getGithubToken();
+
+        // 2. 레포지토리 트리 조회
+        return getRepoBlob(sha, githubUsername, githubReponame, githubToken);
+    }
+
+    /**
+     * 레포지토리의 default branch 조회
+     * @param githubToken: 깃허브 토큰
+     * @param githubUsername: 깃허브 사용자 이름
+     * @param githubReponame: 깃허브 레포지토리 이름
+     * @return default branch 이름
+     */
+    private String getDefaultBranch(String githubToken, String githubUsername, String githubReponame) {
+        String repoUrl = "https://api.github.com/repos/" + githubUsername + "/" + githubReponame;
+
+        // 1. 해당 레포 특정 조회 (default branch 를 가져옴)
+        try{
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(githubToken);
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            HttpEntity<Void> request = new HttpEntity<>(headers);
+            ResponseEntity<Map> response = restTemplate.exchange(
+                    repoUrl,
+                    HttpMethod.GET,
+                    request,
+                    Map.class
+            );
+
+            Map<String, Object> responseBody = response.getBody();
+
+            return (String) responseBody.get("default_branch");
+        } catch (Exception e) {
+            throw new RuntimeException("Error fetching default branch: " + e.getMessage());
+        }
+    }
+
+    /**
+     * 레포지토리의 트리 조회
+     * @param branch: default 브랜치 이름
+     * @param githubUsername: 깃허브 사용자 이름
+     * @param githubReponame: 깃허브 레포지토리 이름
+     * @return 레포지토리 트리 정보
+     */
+    private Object getRepoTrees(String branch, String githubUsername, String githubReponame,String githubToken) {
+        String branchUrl = UriComponentsBuilder
+                .fromHttpUrl("https://api.github.com/repos/" + githubUsername + "/" + githubReponame + "/git/trees/" + branch)
+                .queryParam("recursive", "1")
+                .build()
+                .toUriString();
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(githubToken);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+        ResponseEntity<Map> response = restTemplate.exchange(
+                branchUrl,
+                HttpMethod.GET,
+                request,
+                Map.class
+        );
+
+        return response.getBody();
+    }
+
+    private BlobResponseDTO getRepoBlob(String sha, String githubUsername, String githubReponame,String githubToken) {
+        String blobUrl = "https://api.github.com/repos/" + githubUsername + "/" + githubReponame + "/git/blobs/" + sha;
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(githubToken);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+        ResponseEntity<BlobResponseDTO> response = restTemplate.exchange(
+                blobUrl,
+                HttpMethod.GET,
+                request,
+                BlobResponseDTO.class
+        );
+
+        return response.getBody();
+    }
 
     private ProjectResponseDTO convertToDTO(Project project) {
         ProjectResponseDTO dto = new ProjectResponseDTO();
