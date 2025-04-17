a01d877fe197719a9407c635d15622752152c45f
kyj0503 <heroria0503@gmail.com>
2025-04-18 01:59:50 +0900

    FEAT: 회의 내용 바탕으로 테이블 추가

--- src/main/java/com/webproject/jandi_ide_backend/user/entity/User.java
+++ src/main/java/com/webproject/jandi_ide_backend/user/entity/User.java
@@ -8,11 +8,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 
-/**
- * 사용자(User) 정보를 표현하는 엔티티 클래스
- * 깃허브 계정을 통한 로그인, 프로필 이미지, 자기소개, 닉네임, 이메일 등의 정보를 관리
- * 선호 언어와 관심 기업은 별도의 다대다 관계 조인 테이블(UserTechStack, UserFavoriteCompany)을 통해 관리
- */
+// 사용자(User) 정보를 표현하는 엔티티 클래스
 @Entity
 @Table(name = "users")
 @Getter
@@ -20,7 +16,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User {
 
-    // 고유 식별자 (AUTO_INCREMENT)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -53,12 +48,10 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-    // 엔티티 생성 시 자동으로 현재 시각이 기록 (수정 불가능)
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    // 엔티티 업데이트 시 자동으로 현재 시각으로 갱신
     @UpdateTimestamp
     @Column(nullable = false)
     private LocalDateTime updatedAt;
