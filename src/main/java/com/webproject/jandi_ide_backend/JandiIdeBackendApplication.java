c53bf395f44e946387e67bfce702b19032bc4bae
kyj0503 <heroria0503@gmail.com>
2025-08-25 20:23:53 +0900

    fix: TomcatMetric 오류 해결

--- src/main/java/com/webproject/jandi_ide_backend/JandiIdeBackendApplication.java
+++ src/main/java/com/webproject/jandi_ide_backend/JandiIdeBackendApplication.java
@@ -3,8 +3,12 @@ package com.webproject.jandi_ide_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.web.tomcat.TomcatMetricsAutoConfiguration;
 
-@SpringBootApplication(exclude = {SystemMetricsAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+    SystemMetricsAutoConfiguration.class,
+    TomcatMetricsAutoConfiguration.class
+})
 public class JandiIdeBackendApplication {
 
     public static void main(String[] args) {
