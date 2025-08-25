ac98e9e5f56194766ff94bde96321f9af40456ce
kyj0503 <heroria0503@gmail.com>
2025-08-25 20:19:13 +0900

    fix: ProcessMetric 관련 에러 해결

--- src/main/java/com/webproject/jandi_ide_backend/JandiIdeBackendApplication.java
+++ src/main/java/com/webproject/jandi_ide_backend/JandiIdeBackendApplication.java
@@ -2,8 +2,9 @@ package com.webproject.jandi_ide_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SystemMetricsAutoConfiguration.class})
 public class JandiIdeBackendApplication {
 
     public static void main(String[] args) {
